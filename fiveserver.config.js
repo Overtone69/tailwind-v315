5c589b8e1a056f0afb24b0de91c5f79f73086bde
Roman <info@ahdesign.de>
2022-07-09 23:46:53 -0400

    Erste Version

---
+++ fiveserver.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  highlight: true,
+  injectBody: true,
+  navigate: true,
+  port: 5556,
+  php: "D:\\xampp73\\php\\php.exe"
+}
---
+++ src/test.php
@@ -0,0 +1,3 @@
+<?php
+echo "<p class='text-yellow-500'>Test PHP</p>";
+echo "Test GIT";
